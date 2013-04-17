d530a081efdad2d25d28a35586b8684f570c23f7
grrrwaaa <grrrwaaa@gmail.com>
2013-04-17 15:38:25 +0900

    another example

--- agent_flowdirfield.lua
+++ agent_flowdirfield.lua
@@ -3,20 +3,18 @@ local draw2D = require "draw2D"
 local win = require "window"
 local vec2 = require "vec2"
 
-win:setdim(600, 600)
-math.randomseed(os.time())
+win:setdim(500, 500)
+--math.randomseed(os.time())
+
 
 local dimx = 128
 local dimy = dimx
--- flow field in X and Y components:
-local fx = field2D.new(dimx, dimy)
-local fy = field2D.new(dimx, dimy)
-local fx_old = field2D.new(dimx, dimy)
-local fy_old = field2D.new(dimx, dimy)
+local field = field2D.new(dimx, dimy)
 
 function field_reset()
-	fx:set(function() return math.random()*2-1 end)
-	fy:set(function() return math.random()*2-1 end)
+	field:set(function() return math.random() end)
+	field:diffuse(field, 1)
+	field:normalize()
 end
 field_reset()
 
@@ -34,7 +32,7 @@ end
 
 local agents = {}
 function reset_agents()
-	for i = 1, 200 do
+	for i = 1, 2000 do
 		agents[i] = {
 			pos = vec2(math.random(), math.random()),
 			vel = vec2(),
@@ -46,25 +44,15 @@ end
 reset_agents()
 
 function update()
-	
-	---[[
-	-- swap fields:
-	fx, fx_old = fx_old, fx
-	fy, fy_old = fy_old, fy
-	-- diffuse fields:
-	fx:diffuse(fx_old, 0.001)
-	fy:diffuse(fy_old, 0.001)
-	--]]
-	
-	-- read field
 	for i, a in ipairs(agents) do
-		local x = fx:sample(a.pos.x, a.pos.y)
-		local y = fy:sample(a.pos.x, a.pos.y)
-		local c = vec2(x, y):angle()
+		local c = field:sample(a.pos.x, a.pos.y)
 		-- introduce noise:
-		c = c + (math.random() - 0.5)*0.5
-		a.direction = c
+		--c = c + (math.random() - 0.5)*0.2
+		-- feedback to field:
+		--field:update(c, a.pos.x, a.pos.y)
 		
+		-- convert to direction (radians):
+		a.direction = c * math.pi * 2
 		-- apply movement:
 		a.vel = vec2.fromPolar(0.01, a.direction)
 		-- locomotion:
@@ -72,25 +60,10 @@ function update()
 		-- boundary:
 		a.pos:mod(1) 
 	end
-	
-	-- update field
-	for i, a in ipairs(agents) do	
-		
-		-- feedback to field:
-		local x = math.cos(a.direction)
-		local y = math.sin(a.direction)
-		fx:update(x, a.pos.x, a.pos.y)
-		fy:update(y, a.pos.x, a.pos.y)
-	end
 end
 
 function draw()
-	local gl = require "gl"
-	gl.LineWidth(0.5)
-
-	draw2D.color(1, 1, 1)
-	field2D.drawFlow(fx, fy)
-	
+	field:draw()
 	for i, a in ipairs(agents) do
 		draw_agent_boxily(a)
 	end
