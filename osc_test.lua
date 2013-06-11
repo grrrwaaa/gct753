c33fe1f439db62dcef7f1def10ac4dc84304a37f
grrrwaaa <grrrwaaa@gmail.com>
2013-06-11 12:22:52 +0900

    latest

--- osc_test.lua
+++ osc_test.lua
@@ -7,3 +7,19 @@ print(s:port(), s:ip(), s:host())
 
 -- send a message:
 s:send("/foo", 1, 2.2, "three")
+
+
+
+-- receiver:
+local r = osc.Recv(10002)
+
+function update()
+	for msg in r:recv() do
+		print(msg.addr, #msg)
+		local freq = msg[1]
+		print(freq)
+	end	
+	
+	
+	
+end
