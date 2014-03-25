eabbed8da07d0b599ffff5ab444c9907bf24b4ef
grrrwaaa <grrrwaaa@gmail.com>
2014-03-25 11:46:50 +0900

    ants with sound!

--- js/al.libs.js
+++ js/al.libs.js
@@ -5,6 +5,7 @@ npm install ndarray zeros
 // etc.
 
 browserify js/gibberish_2.0.min.js js/al.libs.js  -o al.js && minify al.js al.min.js
+browserify js/al.libs.js  -o al.js && minify al.js al.min.js
 */
 
 ndarray = require("ndarray");
@@ -39,13 +40,202 @@ wrap = function (n, m) {
 // the exported module:
 al = {};
 
-var al_rendering = false;
-var al_now = Date.now();
-var al_dt = 1/60.;
+al.fps = 30;
 
+var al_rendering = false;
+var al_update_in_audio = false;
+var al_now = Date.now();
+var al_dt = 1/al.fps;
+
+var hz2table = 1024 / 44100;
+
+al.audio = {
+	bufferSize: 4096,
+	sampleRate: 44100,
+	sampleRateInverse: 1/44100,
+	outchannels: 2,
+	context: null,
+	node: null,
+	
+	tableSize: 1024,
+	
+	samples2radians: Math.PI * 2 / 44100,
+	latency: 0.1,
+	
+	t: 0,
+	dt: 1/10,
+	nextUpdate: 0,
+}
+
+var tableSine = new Float32Array(al.audio.tableSize + 1);
+for (var i = 0; i <= al.audio.tableSize; i++) { tableSine[i] = Math.sin((Math.PI * 2.0 * i)/al.audio.tableSize); }
+
+var floor = Math.floor;
+
+var ugens = [];
+
+al.audio.SinOsc = function() {
+	al.audio_init();
+	
+	this.amp = 0.1;
+	this.phase = 0;
+	this.frequency = 440.0;
+	this.table = tableSine;
+	this.pan = 0.5;
+	
+	this.smooth = 0.1;
+	
+	this.freqsmooth = this.frequency;
+	this.last = 0;
+	
+	// start playing immediately
+	ugens.push(this);
+}
+
+al.audio.SinOsc.prototype.connect = function() {
+	ugens.push(this);
+	return this;
+}
+
+al.audio.SinOsc.prototype.next = function() {
+
+
+	var phase = this.phase;
+	var table = this.table;
+	var amp = this.amp;
+	
+	this.freqsmooth += this.smooth * (this.frequency - this.freqsmooth);
+	
+	var pincr = this.freqsmooth * hz2table;
+	
+	phase += pincr;
+	if (phase >= 1024.0) phase = (phase - 1024.0); 
+	
+	var index = floor(phase);
+	var frac = phase - index;
+	 
+	// linear interp:
+	var index2, index1 = (~~index);
+	if((index1 | 0) == (1024 | 0)) {
+		index2 = 0
+	} else { 
+		index2 = (index1 + 1) | 0;
+	}
+	var val1 = table[ index1 ];
+	var val2 = table[ index2 ];
+	this.last = amp * (val1 + (frac * (val2 - val1)));
+	this.phase = phase;
+	return this.last;
+}
+
+al.audio.audioProcess = function(event) {
+	
+	var outDataL = event.outputBuffer.getChannelData(0),
+		outDataR = event.outputBuffer.getChannelData(1);
+	var bufferSize = event.outputBuffer.length;
+	
+	var sampleRateInverse = al.audio.sampleRateInverse;
+	
+	//var samples2radians = al.audio.samples2radians;
+	var hz2table = hz2table;
+	
+	var table = tableSine;
+	
+	var t = al.audio.t;
+
+	
+	// Loop through the samples
+	for (var i = 0; i < bufferSize; i++) {
+	
+		// update scene:
+		if (t >= al.audio.nextUpdate) {
+			if (update && typeof update === "function") update(al.audio.dt);
+			al.audio.nextUpdate += 1/al.fps;
+		}
+		
+		// loop over ugens:
+		var l = 0, r = 0;
+		for (var u = 0, ul = ugens.length; u < ul; u++) {
+			var ugen = ugens[u];
+			var v = ugen.next();
+			
+			// pan: 
+			// TODO: equal power pan!
+			r += ugen.pan * v;
+			l += (1.0 - ugen.pan) * v;
+		}
+		
+		// Set the data in the output buffer for each sample
+		outDataL[i] = l;
+		outDataR[i] = r;
+		
+		t += sampleRateInverse;
+	}
+	
+	al.audio.t += bufferSize * sampleRateInverse;
+}
+
+var audioContext;
+if( typeof webkitAudioContext !== 'undefined' ) {
+	audioContext = webkitAudioContext
+} else if ( typeof AudioContext !== 'undefined' ) {
+	audioContext = AudioContext
+}
+
+
+
+// returns false if audio is not supported.
+// audio might not start immediately however; check al.audio.context != null
+al.audio_init = function() {
+	if (al_update_in_audio) { return true; }
+	
+	// this would need to be deferred for iOS devices, since audio can only be started in response to a user event
+	al.audio_start = function() {
+		if( typeof audioContext !== 'undefined' ) {
+			document.getElementsByTagName('body')[0].removeEventListener('touchstart', al.audio_start);
+			al.audio.context = new audioContext();
+			al.audio.sampleRate = al.audio.context.sampleRate;
+			al.audio.node = al.audio.context.createScriptProcessor(al.audio.bufferSize, 0, al.audio.outchannels, al.audio.context.sampleRate);	
+			al.audio.node.onaudioprocess = al.audio.audioProcess;
+			al.audio.node.connect(al.audio.context.destination);
+			
+			al.audio.sampleRateInverse = 1 / al.audio.context.sampleRate;
+			al.audio.samples2radians = Math.PI * 2 / al.audio.context.sampleRate;
+			hz2table = al.audio.tableSize / al.audio.context.sampleRate;
+
+			if('ontouchstart' in document.documentElement) { // required to start audio under iOS 6
+				var mySource = al.audio.context.createBufferSource();
+				mySource.connect(al.audio.context.destination);
+				mySource.noteOn(0);
+			}
+			return true;
+			
+		} else if ( navigator.userAgent.indexOf( 'Firefox' ) === -1 ) {
+			al.audio.AudioDataDestination(44100, Gibberish.audioProcessFirefox);
+			al.audio.context = { sampleRate: 44100 }; // needed hack to determine samplerate in ugens
+			return true;
+			
+		} else {
+			alert('Your browser does not support javascript audio synthesis. Please try downloading a recent Chrome, FireFox or Safari (for example).');
+			return false;
+		}
+	}
+	
+	if('ontouchstart' in document.documentElement) {
+		document.getElementsByTagName('body')[0].addEventListener('touchstart', al.audio_start);
+		al_update_in_audio = true;
+	} else {
+		al_update_in_audio = al.audio_start();
+	}
+	console.log("starting audio", al_update_in_audio);
+	return al_update_in_audio;
+}
 
 al.once = function() {
-	if (update && typeof update === "function") update(al_dt);
+	if ((!al_update_in_audio) && update && typeof update === "function") {
+		update(al_dt);
+		console.log("render update");
+	}
 	if (draw && typeof draw === "function") draw();
 }
 
@@ -73,7 +263,7 @@ al.stop = function() {
 var canvas, ctx;
 var offscreen_canvas, offscreen_ctx, offscreen_image, offscreen_data;
 
-al.init = function () {
+al.init = function (options) {
 	canvas = document.createElement("canvas");
 	canvas.setAttribute("width", 512);
 	canvas.setAttribute("height", 256);
@@ -110,6 +300,13 @@ al.init = function () {
 	b.innerHTML = 'reset';
 	b.onclick = function(){ reset(); draw(); return false; };
 	document.body.appendChild(b);
+	
+	if (options) {
+		if (options.audio) {
+			al_update_in_audio = al.audio_init();
+			console.log("enabling audio:", al_update_in_audio);
+		}
+	}
 }
 
 field2D = function (width, height) {
